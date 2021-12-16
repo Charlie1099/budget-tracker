e01f0a0943356955209eef5917152b2de2904483
charlie1099 <cdcharlesworth@gmail.com>
2021-12-15 21:21:28 -0700

    added offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -40,5 +40,36 @@ function uploadTransaction() {
 
     const getAll = transactionObjectStore.getAll()
 
-    
-}
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then((response) => response.json())
+          .then((serverResponse) => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+            // open one more transaction
+            const transaction = db.transaction(["new_transaction"], "readwrite");
+            // access the object store
+            const transactionObjectStore = transaction.objectStore("new_transaction");
+            // clear all items in your store
+            transactionObjectStore.clear();
+  
+            alert("All saved transactions has been submitted!");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    };
+  }
+  
+  // listen for app coming back online
+  window.addEventListener("online", uploadTransaction);
